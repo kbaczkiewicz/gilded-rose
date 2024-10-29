bbdcfce71a9428ba829a7e541203ea2093cc2d90
Kamil Bączkiewicz <kbaczkiewitz@gmail.com>
2024-10-29 18:06:27 +0100

    AgedBrie representation as an object

---
+++ src/AgedBrie.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+namespace App;
+
+class AgedBrie extends Item
+{
+    public function __construct(string $name, int $sellIn, int $quality)
+    {
+        parent::__construct($name, $sellIn, $quality);
+    }
+
+    public function advance(): void
+    {
+        $this->sellIn -= 1;
+        if ($this->quality < 50) {
+            if ($this->sellIn < 0) {
+                $this->upgrade(2);
+            } else {
+                $this->upgrade(1);
+            }
+        }
+    }
+
+    protected function upgrade(int $quality): void
+    {
+        $this->quality = min($this->quality + $quality, 50);
+    }
+}
---
+++ tests/AgedBrieTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+use App\AgedBrie;
+
+class AgedBrieTest extends \PHPUnit\Framework\TestCase
+{
+    public function dataProvider(): \Generator
+    {
+        yield 'Advancing AgedBrie upgrades quality and decreases sellIn' => ['Brie', 3, 30, 2, 31];
+        yield 'Advancing AgedBrie upgrades quality by 2 and decreases sellIn (into negative values)' => ['Brie', 0, 30, -1, 32];
+        yield 'Advancing AgedBrie with maximum quality does not increase it further' => ['Brie' ,0 , 50, -1, 50];
+        yield 'Advancing AgedBrie until maximum quality' => ['Brie', 0, 49, -1, 50];
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testAdvancingSetsCorrectValues(
+        string $name,
+        int $sellIn,
+        int $quality,int $targetSellIn,
+        int $targetQuality
+    )
+    {
+        $item = new AgedBrie($name, $sellIn, $quality);
+
+        $item->advance();
+
+        $this->assertEquals($targetSellIn, $item->getSellIn());
+        $this->assertEquals($targetQuality, $item->getQuality());
+    }
+}
